852db94c0e5fc99f2ab78ffe78ba8eaa13bb3f76
Eduard <balteanueduard3@gmail.com>
2025-09-22 19:29:21 +0200

    fix

--- EsempioBar/src/main.ts
+++
@@ -1,8 +0,0 @@
-import "./style.css";
-
-const button = document.querySelector(".btn");
-const target = document.querySelector<HTMLElement>("#menu");
-
-button?.addEventListener("click", () => {
-  target?.scrollIntoView({ behavior: "smooth" });
-});
--- EsempioBar/vite.config.ts
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  base: "/esempio_bar/", // qui puoi mettere "/nome-repo/" quando pubblichi su GitHub Pages
-})
